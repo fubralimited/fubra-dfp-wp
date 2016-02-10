119005862b8e1832c677bda0a7d7d6d96cdbe0bb
Ray Viljoen <ray@fubra.com>
2016-02-10 13:30:53 +0200

    Adds support for custom size map

--- dfw.php
+++ dfw.php
@@ -18,25 +18,47 @@ $DoubleClick->register_breakpoint('large', array('minWidth'=>1200,'maxWidth'=>99
 // Get json file with size configurations
 $sizeMap = json_decode( file_get_contents( dirname(__FILE__) . '/sizemap.json' ), TRUE );
 
+// Internal helper function
+function _str_clean($s) {
+  $s = strtolower($s);
+  $s = str_replace(' ', '', $s);
+  return $s;
+}
+
 // Template function
-function dfp( $ad, $type, $map ) {
+function dfp( $ad, $type, $user_map ) {
   global $DoubleClick, $sizeMap;
 
+  // Error html
+  $err  = '<h1 style="color:white;background:red;padding:10px;font-size:25px;">';
+  $err .= 'DFP AD ERROR - ';
+  $err .= '<a style="color:white;text-decoration:underline;" target="_blank" href="https://github.com/fubralimited/fubra-dfp-wp">';
+  $err .= 'Check Documentation</a></h1>';
+
+  // Check ad code is set
+  $ad = _str_clean($ad);
+  if( !$ad ) return $err;
+
+  // Start with empty size map
   $map = array();
 
-  if (isset($type)) {
+  // Check if a custom map was defined
+  if ( isset($user_map) ) {
 
-    $type = strtolower($type);
-    $type = str_replace(' ', '', $type);
-    $map =$sizeMap[$type];
+    $user_map = _str_clean($user_map);
+    $map = explode( ',', $user_map );
+    $map = array_combine( array('small','medium','large'), $map );
 
-  } else {
-    $err  = '<h1 style="color:white,background:red,padding:10px,font-size:25px;">';
-    $err .= 'DFP AD ERROR - <a href="https://github.com/fubralimited/fubra-dfp-wp">Check Documentation</a>';
-    $err .= '</h1>';
-    return $err;
-  }
+  // Format size string if set
+  } else if ( isset($type) ) {
 
+    $type = _str_clean($type);
+    $map = $sizeMap[$type];
+
+  // Else return err
+  } else { return $err; }
+
+  // Return ad code
   return $DoubleClick->place_ad( $ad, $map );
 }
 
