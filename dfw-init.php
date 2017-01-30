595c9edb4af54aec8e13bc5056c231244920ae5b
Ray Viljoen <ray@fubra.com>
2017-01-30 14:08:01 +0200

    Fixes plugin to not be WAC specific

--- dfw-init.php
+++ dfw-init.php
@@ -94,7 +94,7 @@ class DoubleClick {
 		// Script enqueue is static because we only ever want to print it once.
 		if(!$this::$enqueued) {
 			add_action('wp_footer', array($this, 'enqueue_scripts'));
-			$this::$enqueued = true;
+			$this->enqueued = true;
 		}
 
 		add_action('wp_print_footer_scripts', array($this, 'footer_script'));
--- dfw.php
+++ dfw.php
@@ -2,161 +2,67 @@
 /*
 Plugin Name: 	Fubra DoubleClick for WordPress
 Description: 	Responsive DoubleClick ads in WordPress. Modified copy of https://wordpress.org/plugins/doubleclick-for-wp/
-Author:    		Fubra Ltd.
-Version: 		  1.1.0
+Version: 		  1.0.0
 */
+
+global $DoubleClick;
 
 include('dfw-init.php');
 
-function google_ads () {
 
-    global  $wp_query,
-            $google_ads_unit,
-            $post,
-            $DoubleClick,
-            $listing_page,
-            $blog;
+// Define breakpoints
+$DoubleClick->register_breakpoint('small', array('minWidth'=> 0,'maxWidth'=>511));
+$DoubleClick->register_breakpoint('medium', array('minWidth'=>512,'maxWidth'=>1199));
+$DoubleClick->register_breakpoint('large', array('minWidth'=>1200,'maxWidth'=>99999));
 
-    $google_ads_header = '';
-    $google_ads_pub    = 'ca-pub-5419175785675578';
-    $data              = new stdClass();
+// Get json file with size configurations
+$sizeMap = json_decode( file_get_contents( dirname(__FILE__) . '/sizemap.json' ), TRUE );
 
-    if( class_exists( 'WAC_Search' ) && WAC_Search::is_search_page() ) {
-
-        $data->sidebar = 'WAC_Searchresultspages_ATF_Skyscraper_A';
-        $data->footer  = 'WAC_Searchresultspages_BTF_Footer_728x90_A';
-
-    } elseif( $listing_page ) {
-
-        $data->sidebar       = 'WAC_Alphabeticalpages_ATF_Skyscraper_A';
-        $data->footer        = 'WAC_Alphabeticalpages_BTF_Footer_728x90_A';
-        $data->leaderboard_1 = 'WAC_Alphabeticalpages_BTF_Leaderboard_A';
-        $data->leaderboard_2 = 'WAC_Alphabeticalpages_BTF_Leaderboard_2_A';
-        $data->leaderboard_3 = 'WAC_Alphabeticalpages_BTF_Leaderboard_3_A';
-
-    } elseif( is_page_template('templates/directory-taxi.php') ) {
-
-        $data->body    = 'WAC_TaxiPages_ATF_Leaderboard';
-        $data->sidebar = 'WAC_TaxiPages_ATF_Right_160x600';
-        $data->footer  = 'WAC_TaxiPages_BTF_Footer_728x90';
-
-    } elseif( is_page_template('templates/directory-hotel.php') ) {
-
-        $data->body    = 'WAC_HotelPages_ATF_Leaderboard';
-        $data->sidebar = 'WAC_HotelPages_ATF_Right_160x600';
-        $data->footer  = 'WAC_HotelPages_BTF_Footer_728x90';
-
-    } elseif( is_page_template('templates/directory-parking.php') ) {
-
-        $data->body    = 'WAC_ParkingPages_ATF_Leaderboard';
-        $data->sidebar = 'WAC_ParkingPages_ATF_Right_160x600';
-        $data->footer  = 'WAC_ParkingPages_BTF_Footer_728x90';
-
-    } elseif( is_page_template('templates/directory-carhire.php') ) {
-
-        $data->body    = 'WAC_CarHirePages_ATF_Leaderboard';
-        $data->sidebar = 'WAC_CarHirePages_ATF_Right_160x600';
-        $data->footer  = 'WAC_CarHirePages_BTF_Footer_728x90';
-
-    }  elseif( is_page_template('templates/directory-currency.php') ) {
-
-        $data->body    = 'WAC_CarHirePages_ATF_Leaderboard';
-        $data->sidebar = 'WAC_CarHirePages_ATF_Right_160x600';
-        $data->footer  = 'WAC_CarHirePages_BTF_Footer_728x90';
-
-    } elseif( get_post_type() === 'airport' ) {
-
-        // Top banner
-        $data->top_leaderboard       = 'WAC_AirportResultsPage_ATF_Leaderboard';
-
-        // Between the hotel and affiliates links and the destinations
-        $data->affiliate_leaderboard = 'WAC_AirportResultsPage_BTF_Leaderboard';
-
-        // Skyscraper
-        $data->sidebar               = 'WAC_AirportResultsPage_ATF_Right_160x600';
-
-        // Only on long pages, placed under gallery
-        $data->gallery_leaderboard   = 'WAC_AirportResultsPage_BTF_Leaderboard_2';
-
-        // Test Ads Sales - Dubai
-        $data->DXB_leaderboard       = 'WAC_DXBairport_ATF_Leaderboard';
-
-
-
-        foreach( $data as $key => $value ) {
-            $data->{$key} = $value.'_B';
-        }
-
-        // Overide leaderboard on airport pages
-        $data->IST_leaderboard   = 'WAC_ISTairport_ATF_Leaderboard';
-        $data->IST_leaderboard   = 'WAC_ISTairport_ATF_Leaderboard';
-    		$data->PEK_leaderboard   = 'WAC_PEKairport_ATF_Leaderboard';
-    		$data->PVG_leaderboard   = 'WAC_PVGairport_ATF_Leaderboard';
-        $data->HKG_leaderboard   = 'WAC_HKGairport_ATF_Leaderboard';
-        $data->SIN_leaderboard   = 'WAC_SIN_airport_ATF_Leaderboard';
-        $data->KUL_leaderboard   = 'WAC_KUL_airport_ATF_Leaderboard';
-        $data->BKK_leaderboard   = 'WAC_BKK_airport_ATF_Leaderboard';
-        $data->BNE_leaderboard   = 'WAC_BNE_airport_ATF_Leaderboard';
-        $data->SHA_leaderboard   = 'WAC_SHA_airport_ATF_Leaderboard';
-        $data->CGK_leaderboard   = 'WAC_CGK_airport_ATF_Leaderboard';
-        $data->SGN_leaderboard   = 'WAC_SGN_airport_ATF_Leaderboard';
-
-
-    } elseif( in_array(get_the_title(), array('World Top 30 Airports', 'US Top 40 Airports')) ) {
-
-        $data->sidebar           = 'WAC_Contentpages_ATF_Skyscraper_C';
-        $data->BTF_Leaderboard   = 'WAC_Contentpages_BTF_Leaderboard_C';
-        $data->BTF_Leaderboard_2 = 'WAC_Contentpages_BTF_Leaderboard_2_C';
-        $data->BTF_Leaderboard_3 = 'WAC_Contentpages_BTF_Leaderboard_3_C';
-        $data->BTF_Footer_728x9  = 'WAC_Contentpages_BTF_Footer_728x9_C';
-
-    } elseif( in_array(get_the_title(), array('UK Top 20 Airports')) ) {
-
-        $data->sidebar           = 'WAC_UKTop20_ATF_WideSkyscraper';
-        $data->BTF_Leaderboard   = 'WAC_UKTop20_BTF_Leaderboard';
-        $data->BTF_Leaderboard_2 = 'WAC_Contentpages_BTF_Leaderboard_2_C';
-        $data->BTF_Leaderboard_3 = 'WAC_Contentpages_BTF_Leaderboard_3_C';
-        $data->BTF_Footer_728x9  = 'WAC_UKTop20_BTF_Footer_728x90';
-
-    } elseif( $blog ) {
-
-        $data->blog_atf = 'WAC_Blog_ATF_Leaderboard';
-        $data->blog_btf = 'WAC_Blog_BTF_Footer';
-
-    } else {
-
-        $data->sidebar = 'WAC_HomepageAndContentPages_BTF_Right_300x250';
-        $data->mpu = 'WAC_HomepageAndContentPages_BTF_Right_300x250';
-        $data->blb = 'WAC_BTF_Footer_728x90'; // Leader board at the bottom
-
-    }
-
-    if( !isset($data->blb) || !isset($data->footer) ) {
-        // Leader board at the bottom
-        $data->blb = 'WAC_BTF_Footer_728x90';
-    }
-
-    if( !is_object( $google_ads_unit ) ) {
-        $google_ads_unit = new stdClass;
-
-    }
-
-    foreach ( $data as $type => $value) {
-
-        if($type !== 'sidebar') {
-          $google_ads_unit->{$type} = $DoubleClick->place_ad( $value, array( 'small' => '320x50', 'medium' => '468x60', 'large' => '728x90' ) );
-        } else {
-          $google_ads_unit->{$type} = $DoubleClick->place_ad( $value, array( 'small' => '160x600', 'medium' => '160x600','large' => '160x600' ) );
-        }
-    }
+// Internal helper function
+function _str_clean($s) {
+  $s = strtolower($s);
+  $s = str_replace(' ', '', $s);
+  return $s;
 }
 
-add_shortcode('dfp_leaderboard', function($args){
-  global $DoubleClick;
-  return $DoubleClick->place_ad( $args['ad'], array( 'small' => '320x50', 'medium' => '468x60', 'large' => '728x90' ) );
-});
+// Template function
+function dfp( $ad, $type, $user_map ) {
+  global $DoubleClick, $sizeMap;
 
-add_shortcode('dfp_skyscraper', function($args){
-  global $DoubleClick;
-  return $DoubleClick->place_ad( $args['ad'], array( 'small' => '160x600', 'medium' => '160x600','large' => '160x600' ) );
+  // Error html
+  $err  = '<h1 style="color:white;background:red;padding:10px;font-size:25px;">';
+  $err .= 'DFP AD ERROR - ';
+  $err .= '<a style="color:white;text-decoration:underline;" target="_blank" href="https://github.com/fubralimited/fubra-dfp-wp">';
+  $err .= 'Check Documentation</a></h1>';
+
+  // Check ad code is set
+  $ad = _str_clean($ad);
+  if( !$ad ) return $err;
+
+  // Start with empty size map
+  $map = array();
+
+  // Check if a custom map was defined
+  if ( isset($user_map) ) {
+
+    $user_map = _str_clean($user_map);
+    $map = explode( ',', $user_map );
+    $map = array_combine( array('small','medium','large'), $map );
+
+  // Format size string if set
+  } else if ( isset($type) ) {
+
+    $type = _str_clean($type);
+    $map = $sizeMap[$type];
+
+  // Else return err
+  } else { return $err; }
+
+  // Return ad code
+  return $DoubleClick->place_ad( $ad, $map );
+}
+
+// Shortcode function
+add_shortcode('dfp', function($args){
+  return dfp( $args['ad'], $args['type'], $args['map'] );
 });
